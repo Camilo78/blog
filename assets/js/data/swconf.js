a18d23568e6f4cc1cff5cc1dda98b4529ec40192
camilo78 <camilo.alvarado0501@gmail.com>
2024-10-24 21:47:32 -0600

    first commit

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1729822172',resources: [
+    cacheName: 'chirpy-1729827956',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
